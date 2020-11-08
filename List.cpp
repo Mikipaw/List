3e28d6d86e0273a768fba73cf9f1c2d1220c3952
Mikipaw <71548305+Mikipaw@users.noreply.github.com>
2020-11-09 01:59:02 +0300

    Update List.cpp

--- List.cpp
+++ List.cpp
@@ -7,11 +7,6 @@
 
 
 int List::Insert(int pos, double value) {
-    if(pos > size) return OUT_OF_RANGE;
-
-    if(size == capacity) Expand();
-
-    //size_t new_pos = FindFree(pos);
     size_t new_pos = bhead;
     bhead = data[new_pos].next;
 
@@ -19,20 +14,27 @@ int List::Insert(int pos, double value) {
         data[new_pos].prev = 0;
         data[new_pos].next = -1;
         data[0].next = new_pos;
+        data[new_pos].item = value;
         size++;
         return new_pos;
     }
 
+    if(data[pos].free) return OUT_OF_RANGE;
+
+    if(size == capacity) Expand();
+
+    //size_t new_pos = FindFree(pos);
     data[new_pos].next = data[pos].next;
     data[data[pos].next].prev = new_pos;
     data[pos].next = new_pos;
     data[new_pos].prev = pos;
 
     data[new_pos].item = value;
+    data[new_pos].free = false;
 
     size++;
 
-    if(data[new_pos].prev == 0)  head = new_pos;
+    if(data[new_pos].prev ==  0) head = new_pos;
     if(data[new_pos].next == -1) tail = new_pos;
 
     return new_pos;
@@ -63,8 +65,9 @@ size_t List::FindFree(int pos) const {
 }
 
 void List::Dump() const {
-    FILE* GraphViz = fopen("Graph1.dot", "wb");
-    if(GraphViz == nullptr) fprintf(stderr, "Error opening file!\n");
+    //printf("%lf\t%lf\n", data[0].item, data[1].item);
+    FILE* Graph = fopen("Graph1.dot", "wb");
+    if(Graph == nullptr) fprintf(stderr, "Error opening file!\n");
 
     const char* DEFAULT_TEXT = "digraph G{\n"
                                "edge [color = \"darkgreen\"];\n"
@@ -77,34 +80,37 @@ void List::Dump() const {
                                "nodeend [label = \"<f1> END\"];\n"
                                "node [color = \"#000000\", shape = record];\n"
                                "nodeinvzero [style = \"filled\", fillcolor = \"#FF019A\"];\n"
-                               "nodeinvzero [label = \"<f1> INVALID_ZERO\"];\n";
-    fwrite(DEFAULT_TEXT, sizeof(char), strlen(DEFAULT_TEXT), GraphViz);
+                               "nodeinvzero [label = \"<f1> INVALID_ZERO\"];\n\0";
+    fwrite(DEFAULT_TEXT, sizeof(char), strlen(DEFAULT_TEXT), Graph);
 
     const char* color = "gold";
-    for(int i = 1; i <= size; ++i) {
-        fprintf(GraphViz, "node[color = \"#000000\", shape = record];\n");
+    for(int i = 1; i <= capacity; ++i) {
+        fprintf(Graph, "node[color = \"#000000\", shape = record];\n");
 
         if(i % 2 == 0) color = "gold";
         else color = "grey";
-        fprintf(GraphViz, "node%d[ style = \"filled\", fillcolor = \"%s\"];\n", i, color);
-        fprintf(GraphViz,
+        fprintf(Graph, "node%d[ style = \"filled\", fillcolor = \"%s\"];\n", i, color);
+        fprintf(Graph,
                 "node%d [label=\"<f0> prev (%d)|<f1> num (%d)|<f2> next (%d)|<f3> data (%lf)\"];\n",
-                                           i, data[i].prev, i, data[i].next, data[i].item);
+                i, data[i].prev, i, data[i].next, data[i].item);
         if(data[i].next != -1) {
-            fprintf(GraphViz, "node%d:f2 -> node%d:f1;\n", i, data[i].next);
-            fprintf(GraphViz, "node%d:f0 -> node%d:f1;\n", data[i].next, i);
+            fprintf(Graph, "node%d:f2 -> node%d:f1;\n", i, data[i].next);
+            fprintf(Graph, "node%d:f0 -> node%d:f1;\n", data[i].next, i);
         }
-        else if(data[i].prev != -1) fprintf(GraphViz, "node%d:f2 -> nodeend;\n", i);
+        else if(data[i].prev != -1) fprintf(Graph, "node%d:f2 -> nodeend;\n", i);
     }
-    fprintf(GraphViz, "node%d -> node%d:f1;\n", 0, data[0].next);
+    fprintf(Graph, "node0       -> node%d:f1;\n",   data[0].next);
+    fprintf(Graph, "node%d [style = \"filled\", fillcolor = \"cyan\"];\n", capacity + 1);
+    fprintf(Graph, "node%d [label=\"BORDER\"];\n", capacity + 1);
 
-    fprintf(GraphViz, "\n}");
-    fclose(GraphViz);
+    fprintf(Graph, "\n}");
+    fclose(Graph);
+
 }
 
 int List::Delete_elem(int pos) {
-    if (pos >= capacity) return OUT_OF_RANGE;
-    if (data[pos].prev == -1) return UNAVAILABLE_ELEMENT;
+    if (pos >= capacity)        return OUT_OF_RANGE;
+    if (data[pos].prev == -1)   return UNAVAILABLE_ELEMENT;
 
     data[data[pos].prev].next = data[pos].next;
     data[data[pos].next].prev = data[pos].prev;
@@ -114,6 +120,9 @@ int List::Delete_elem(int pos) {
     data[pos].prev = -1;
 
     data[pos].item = NAN;
+    data[pos].free = true;
+
+    size--;
 
     return ALL_OK;
 }
@@ -132,5 +141,3 @@ int List::List_OK() {
 
     return ALL_OK;
 }
-
-
